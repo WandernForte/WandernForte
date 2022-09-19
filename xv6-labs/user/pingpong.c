8aea40ab475b6d363e71dc6d5020775662f477b4
arisa <a1648639935@gmail.com>
2022-09-19 14:25:11 +0800

    update

--- xv6-labs/user/pingpong.c
+++ xv6-labs/user/pingpong.c
@@ -6,33 +6,5 @@ void print_recv_msg(char* content){
 }
 
 int main(int argc,char* argv[]){
-    // int stat;
-    pid_t pid_c, pid_p;
-    int p2c[2]={1, 0};
-    int c2p[2]={0, 1};
-    int stat;
-    pipe(p2c);
-    // pid_p = getpid();
-    if((pid_c=fork())<0){
-        printf("ERROR!\n");
-    }
-    if(pid_c==0){//child_proc runs
-        //TODO
-        pipe(c2p);
-        char* content="";
-        read(c2p[0], content, 4);
-        print_recv_msg(content);
-        exit(0);
-    }
-    else{//parent proc runs
-        write(p2c[1],"ping",4);
-        wait(&stat);
-        char* content="";
-        read(p2c[0], content, 4);
-        print_recv_msg(content);
-        //TODO
-        exit(0);
-    }
-    // pipewrite(pi_p2c,stat,5);
-    return pid_c+pid_p;
+
 }
